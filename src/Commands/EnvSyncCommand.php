8f4b60f6803ae56b6ff2709bf84ae06e294f7235
Sven Luijten <svenluijten1995@gmail.com>
2019-09-15 13:11:26 +0200

    Add env:sync command

---
+++ src/Commands/EnvSyncCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Sven\FlexEnv\Commands;
+
+use Illuminate\Support\Collection;
+use Symfony\Component\Console\Input\InputArgument;
+
+class EnvSyncCommand extends EnvCommand
+{
+    /** @var string */
+    protected $name = 'env:sync';
+
+    /** @var string */
+    protected $description = 'Add keys that are present in the "example" file to the environment file.';
+
+    public function handle(): int
+    {
+        $example = $this->config($this->argument('file'));
+        $exampleValues = $example->all();
+
+        $real = $this->config();
+        $realValues = $real->all();
+
+        $missingKeys = array_diff_key($exampleValues, $realValues);
+
+        foreach ($missingKeys as $missingKey => $exampleValue) {
+            $real->set($missingKey, $exampleValue);
+        }
+
+        if ($this->option('dry-run')) {
+            $table = Collection::make($real->all())
+                ->map(function ($value, $key) {
+                    return [$key, $value];
+                })
+                ->toArray();
+
+            $this->info('This would add the following variables to the environment file:');
+            $this->table(['Key', 'Value'], $table);
+            $this->info('Run with "--force" to make these changes.');
+
+            return 0;
+        }
+
+        if ($this->option('force') || $this->confirm('Do you want to add '.count($missingKeys).' new variables to the environment file?')) {
+            $real->persist();
+
+            return 0;
+        }
+
+        $this->info('Alright, no changes made.');
+
+        return 1;
+    }
+
+    public function getArguments()
+    {
+        return [
+            ['file', InputArgument::REQUIRED, 'The name of the example file to use.', '.env.example'],
+        ];
+    }
+}
