903d8223c6e70ad6d7a94cc50e8656e4fd48ef23
Sven Luijten <svenluijten1995@gmail.com>
2019-09-15 13:10:17 +0200

    Add '--force' option to env:delete command

--- src/Commands/EnvDeleteCommand.php
+++ src/Commands/EnvDeleteCommand.php
@@ -3,6 +3,7 @@
 namespace Sven\FlexEnv\Commands;
 
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class EnvDeleteCommand extends EnvCommand
 {
@@ -17,17 +18,36 @@ class EnvDeleteCommand extends EnvCommand
         $config = $this->config();
 
         $config->delete($key = $this->key());
-        $config->persist();
 
-        $this->comment("Successfully deleted the entry \"$key\" from your .env file.");
+        if ($this->option('force') || $this->confirm("Are you sure you want to delete \"$key\" from your .env file?")) {
+            $config->persist();
 
-        return 0;
+            $this->comment("Successfully deleted the entry \"$key\" from your .env file.");
+
+            return 0;
+        }
+
+        $this->comment('Alright, no changes made.');
+
+        return 1;
     }
 
     protected function getArguments()
     {
         return [
             ['key', InputArgument::REQUIRED, 'The name of the environment variable to delete.'],
+        ];
+    }
+
+    protected function getOptions()
+    {
+        return [
+            [
+                'force',
+                null,
+                InputOption::VALUE_NONE,
+                'Do not ask for confirmation before deleting the key from the environment file.',
+            ],
         ];
     }
 }
